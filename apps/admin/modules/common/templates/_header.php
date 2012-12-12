85a7aaefe029e20a01b0befb98f95b806503e0cd
emersonbarrion <emerson.barrion@gmail.com>
2012-12-12 20:08:44 +0800

    Changes in admin menu

--- apps/admin/modules/common/templates/_header.php
+++ apps/admin/modules/common/templates/_header.php
@@ -4,6 +4,26 @@
 	<ul id="nav" class="menubar">
 		<li class="menubaritem first"><a href="<?php echo url_for('index/index') ?>">Dashboard</a></li>
 		<li class="menubaritem first"><a href="<?php echo url_for('users/index') ?>">Users</a></li>
+		<li class="menubaritem first"><a href="<?php echo url_for('user/list') ?>">User</a>
+			<ul>
+            	<li class="menuitem"><a href="<?php echo url_for('user/new') ?>">New</li>
+            </ul>
+        </li>    
+		<li class="menubaritem first"><a href="<?php echo url_for('facilitator/list') ?>">Facilitator</a>
+			<ul>
+            	<li class="menuitem"><a href="<?php echo url_for('facilitator/new') ?>">New</li>
+            </ul>
+        </li>
+		<li class="menubaritem first"><a href="<?php echo url_for('court/list') ?>">Unit</a>
+			<ul>
+            	<li class="menuitem"><a href="<?php echo url_for('court/new') ?>">New</li>
+            </ul>
+        </li>
+		<li class="menubaritem first"><a href="<?php echo url_for('reservation/list') ?>">Reservation</a>
+			<ul>
+            	<li class="menuitem"><a href="<?php echo url_for('reservation/new') ?>">New</li>
+            </ul>
+        </li>
 		<?php if($sf_user->isAuthenticated() && $sf_user->hasCredential(array('admin', 'facilitator'), false)): ?>
 		<li class="menubaritem"><a href="<?php echo url_for('index/logout') ?>">Logout</a></li>
 		<?php endif; ?>
