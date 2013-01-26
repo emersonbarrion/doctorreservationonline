1898eca804fb5f168acc006b1bdd83f0694d8cfe
emersonbarrion <emerson.barrion@gmail.com>
2013-01-27 05:32:23 +0800

    View for reservation list

---
+++ apps/frontend/modules/reservation/templates/_list.php
@@ -0,0 +1,35 @@
+<table>
+    <thead>
+        <tr>
+            <th>Title</th>
+            <th>Court Name</th>
+            <th>Start</th>
+            <th>End</th>
+            <th>Hours</th>
+            <th>Amount</th>
+            <th>Payment Status</th>
+            <th>Status</th>
+        </tr>
+    </thead>
+    <tbody>
+    	<?php foreach($myreservationlist as $key => $val): ?>
+        <tr>
+            <td><?php echo $val['title'] ?></td>
+            <td><?php echo $val['CroCourts']['name'] ?></td>
+            <td><?php echo date('F j, Y g:i a', strtotime($val['start'])) ?></td>
+            <td><?php echo date('F j, Y g:i a', strtotime($val['end'])) ?></td>
+            <td><?php echo $val['hours'] ?></td>
+            <td><?php echo $val['amount'] ?></td>
+            <td><?php echo $val['paymentstatus'] ?></td>
+            <td><?php 
+            	if($val['status']){
+            		echo 'Active';
+            	} else {
+            		echo 'Inactive';
+            	}
+            	?>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
